066f1ba44aa513e17902b03fc1cd146955f61f04
Alex-Turing <alexanderhernandez0684@gmail.com>
2025-01-22 12:07:22 -0500

    first commit

---
+++ src/components/ToDoApp.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { TasksList } from './TasksList';
+
+
+export const ToDoApp = () => {
+const [newTask, setNewTask] = useState<string>("");
+const [tasksList, setTasksList] = useState<string[]>([]);
+
+const handleAddTask = () => {
+    if(newTask.trim() === '') {return}
+    setTasksList([...tasksList, newTask]);
+    setNewTask("");
+};
+
+const handleDeleteTask = (index: number) => {
+    setTasksList(tasks => tasks.filter((_, task) => task !== index));
+};
+
+  return (
+    <div>
+        <h1>TODO List</h1>
+        <div>
+            <input
+                type="text"
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+                placeholder='New Task' 
+            />
+            <button onClick={handleAddTask}>Add Task</button>
+            <TasksList tasksList={tasksList} deleteTask={handleDeleteTask} />
+        </div>
+    </div>
+  )
+}
